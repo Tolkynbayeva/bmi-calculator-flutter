4dd5c0cee8c2a4a093017e2d36795fe5f628c0e9
Tolkynbayeva <workfortime37@gmail.com>
2024-01-24 22:11:43 +0600

    change result words

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -27,9 +27,9 @@ class CalculatorBarin {
     if (_bmi >= 25) {
       return 'You have a higher than normal body weight. Try to exercise more.';
     } else if (_bmi < 18.5) {
-      return 'You have a normal body weight. Good job';
-    } else {
       return 'You have a lower than normal body weight. You can eat a bit more';
+    } else {
+      return 'You have a normal body weight. Good job';
     }
   }
 }
